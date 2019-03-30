f8cf5da7dc8d02ab207f693c2fe36eb05354d4b7
Cédric Nisio <cedric.nis@gmail.com>
2019-03-30 11:31:08 +0100

    initial commit

---
+++ src/main/java/fr/destiny/vacuum/VacuumApplication.java
@@ -0,0 +1,13 @@
+package fr.destiny.vacuum;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VacuumApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VacuumApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/destiny/vacuum/VacuumApplicationTests.java
@@ -0,0 +1,16 @@
+package fr.destiny.vacuum;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class VacuumApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
