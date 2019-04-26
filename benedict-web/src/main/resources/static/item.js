cf026fdecd4f22538d35d6be57e254440e5e7e65
Dayde <cedric.nis@gmail.com>
2019-04-26 21:33:06 +0200

    feat(item): better masterwork

--- benedict-web/src/main/resources/static/item.js
+++ benedict-web/src/main/resources/static/item.js
@@ -3,6 +3,7 @@ Vue.component('item', {
 <div class="item-container">
     <div class="item-image" :title="item.name">
         <img :src="'https://www.bungie.net' + item.icon" :alt="item.name" :class="{ masterwork: item.masterwork }" />
+        <img v-if="item.masterwork" src="/masterwork.png" :alt="item.name" :class="{ masterwork: item.masterwork }" />
         <span class="top-right">{{item.powerLevel}}</span>
     </div>
     <div class="item-sockets">
