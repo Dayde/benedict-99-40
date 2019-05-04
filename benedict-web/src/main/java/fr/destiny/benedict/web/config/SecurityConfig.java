d3f3c9b4edb342be932f1cb18fd3e58e4954ca8a
Dayde <cedric.nis@gmail.com>
2019-05-04 14:25:54 +0200

    feat(cache): enable caching for static resources

--- benedict-web/src/main/java/fr/destiny/benedict/web/config/SecurityConfig.java
+++ benedict-web/src/main/java/fr/destiny/benedict/web/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package fr.destiny.benedict.web.config;
 import fr.destiny.benedict.web.interceptor.SecureInterceptor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @Configuration
@@ -11,5 +12,12 @@ public class SecurityConfig implements WebMvcConfigurer {
     @Override
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(new SecureInterceptor());
+    }
+
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/resources/static/**")
+                .addResourceLocations("/resources/static/")
+                .setCachePeriod(31536000);
     }
 }
