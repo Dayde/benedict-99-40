eb6bdd82a8df53e4283ec134227e0aafc8ef1770
Dayde <cedric.nis@gmail.com>
2019-05-05 10:48:27 +0200

    fix(desktop): allow right click on desktop

--- benedict-web/src/main/resources/static/index.js
+++ benedict-web/src/main/resources/static/index.js
@@ -2,13 +2,36 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/service-worker.js');
 }
 
+const isMobile = {
+    Android: function () {
+        return navigator.userAgent.match(/Android/i);
+    },
+    BlackBerry: function () {
+        return navigator.userAgent.match(/BlackBerry/i);
+    },
+    iOS: function () {
+        return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+    },
+    Opera: function () {
+        return navigator.userAgent.match(/Opera Mini/i);
+    },
+    Windows: function () {
+        return navigator.userAgent.match(/IEMobile/i);
+    },
+    any: function () {
+        return (isMobile.Android() || isMobile.BlackBerry() || isMobile.iOS() || isMobile.Opera() || isMobile.Windows());
+    }
+};
+
 // For mobile, prevents context menu event propagation
 // on touchHold to allow tooltips to show
-window.oncontextmenu = function (event) {
-    event.preventDefault();
-    event.stopPropagation();
-    return false;
-};
+if (isMobile.any()) {
+    window.oncontextmenu = function (event) {
+        event.preventDefault();
+        event.stopPropagation();
+        return false;
+    };
+}
 
 Vue.use(VueTippy, {
     theme: 'dark'
